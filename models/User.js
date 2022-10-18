65b3f29dcf1a538aa4ea94c5337dad17793f8699
Julian Longmire <julian@Julians-MacBook-Pro.local>
2022-10-17 20:18:14 -0700

    done

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class User extends Model {}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            unique: true
+        },
+        first_name: {
+            type: DataTypes.VARCHAR(30),
+            allowNull: false
+        },
+        last_name: {
+            type: DataTypes.VARCHAR(30),
+            allowNull: false
+        }, 
+        email: {
+            type: DataTypes.email,
+            allowNull: false,
+            unique: false
+        }, 
+        password: {
+            type: DataTypes.password,
+            allowNull: false,
+            unique: false
+        },
+        dea_id: {
+            type: DataTypes.VARCHAR(20),
+            unique: true
+        }
+    }
+)
